18e7913155ab1f6970063906f1366ec83bf814e0
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-09 16:15:17 +0000

    feat: works out the scores for a 2nd turn

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,3 +1,9 @@
-export function add(x: number, y: number): number {
-  return x + y;
+export type Frame = { turn1: number; turn2: number; runningTotal?: number };
+
+export function totalScore(frames: Frame[]) {
+  const lastFrame = frames[frames.length - 1];
+  const lastRunningTotal = lastFrame.turn1 + lastFrame.turn2;
+  const previousRunningTotal = frames[frames.length - 2].runningTotal;
+  if (previousRunningTotal) return previousRunningTotal + lastRunningTotal;
+  else return lastRunningTotal;
 }
--- tests/bowling.test.ts
+++ tests/bowling.test.ts
@@ -1,7 +1,19 @@
-import {} from "../src/bowling";
+import { totalScore, Frame } from "../src/bowling";
 
-describe("test  function", () => {
-  it("", () => {
-    //expect(add(10, 5)).toBe(15);
+describe("test total score  function", () => {
+  it("it should add the total score for a set of 2 frames at bowling", () => {
+    const frames: Frame[] = [
+      {
+        turn1: 4,
+        turn2: 5,
+        runningTotal: 9,
+      },
+      {
+        turn1: 3,
+        turn2: 4,
+      },
+    ];
+
+    expect(totalScore(frames)).toBe(16);
   });
 });
