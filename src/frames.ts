6475dbcbf57e4374f067e1002662ed0f703d6437
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-14 17:05:31 +0000

    feat: create a set of 11 frames with last frame being 2 turn for scoring strike in 10th frame

--- src/frames.ts
+++ src/frames.ts
@@ -1,25 +1,42 @@
 export function createFrames(framesString: string) {
   const framesAsArray = framesString.split(" ");
   return framesAsArray.map((frame) => {
-    if (frame === "X")
-      return { turn1: Number(10), isSpare: false, isStrike: true };
+    if (frame === "X") return getStrikeFrame();
     else if (framesAsArray.indexOf(frame) === 10)
-      return { turn1: Number(frame), isSpare: false, isStrike: false };
+      return getEleventhFrame(frame);
     else if (frame.includes("-")) {
-      const turns = frame.split("-");
-      const turnsAsNum = turns.map((t) => (t === "" ? 0 : Number(t)));
-      return {
-        turn1: turnsAsNum[0],
-        turn2: turnsAsNum[1],
-        isSpare: false,
-        isStrike: false,
-      };
+      return getScoreForFrameWithMiss(frame);
     } else {
-      const turns = frame.split("/");
-      const turn1 = Number(turns[0]);
-      const turn2 = turns[1] === "" ? 10 - turn1 : Number(turns[1]);
-      const isSpare = turn1 + turn2 === 10;
-      return { turn1: turn1, turn2: turn2, isSpare: isSpare, isStrike: false };
+      return getScoreForFrameTwoTurns(frame);
     }
   });
+}
+
+function getStrikeFrame() {
+  return { turn1: Number(10), isSpare: false, isStrike: true };
+}
+
+function getEleventhFrame(frame: string) {
+  if (frame.includes("/")) {
+    return getScoreForFrameTwoTurns(frame);
+  } else return { turn1: Number(frame), isSpare: false, isStrike: false };
+}
+
+function getScoreForFrameTwoTurns(frame: string) {
+  const turns = frame.split("/");
+  const turn1 = Number(turns[0]);
+  const turn2 = turns[1] === "" ? 10 - turn1 : Number(turns[1]);
+  const isSpare = turn1 + turn2 === 10;
+  return { turn1: turn1, turn2: turn2, isSpare: isSpare, isStrike: false };
+}
+
+function getScoreForFrameWithMiss(frame: string) {
+  const turns = frame.split("-");
+  const turnsAsNum = turns.map((t) => (t === "" ? 0 : Number(t)));
+  return {
+    turn1: turnsAsNum[0],
+    turn2: turnsAsNum[1],
+    isSpare: false,
+    isStrike: false,
+  };
 }
