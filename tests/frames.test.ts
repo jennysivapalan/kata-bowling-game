b44b0c821ec8cf3ffa12d635e11a34027fb4d087
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-15 09:08:39 +0000

    test: 11th frame is a spare when 10th frame is a strike

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -434,6 +434,79 @@ describe("test createFrames function", () => {
     expect(createFrames(framesAsString)).toEqual(expectedFrames);
   });
 
+  it("it should create a set of 11 frames with last frame being a spare for scoring strike in 10th frame", () => {
+    const framesAsString = "5/3 -- 5/ X 3/ 3/ 5/2 2- 5/4 X 3/";
+
+    const expectedFrames: Frame[] = [
+      {
+        turn1: 5,
+        turn2: 3,
+        isSpare: false,
+        isStrike: false,
+      },
+      {
+        turn1: 0,
+        turn2: 0,
+        isSpare: false,
+        isStrike: false,
+      },
+      {
+        turn1: 5,
+        turn2: 5,
+        isSpare: true,
+        isStrike: false,
+      },
+      {
+        turn1: 10,
+        isSpare: false,
+        isStrike: true,
+      },
+      {
+        turn1: 3,
+        turn2: 7,
+        isSpare: true,
+        isStrike: false,
+      },
+      {
+        turn1: 3,
+        turn2: 7,
+        isSpare: true,
+        isStrike: false,
+      },
+      {
+        turn1: 5,
+        turn2: 2,
+        isSpare: false,
+        isStrike: false,
+      },
+      {
+        turn1: 2,
+        turn2: 0,
+        isSpare: false,
+        isStrike: false,
+      },
+      {
+        turn1: 5,
+        turn2: 4,
+        isSpare: false,
+        isStrike: false,
+      },
+      {
+        turn1: 10,
+        isSpare: false,
+        isStrike: true,
+      },
+      {
+        turn1: 3,
+        turn2: 7,
+        isSpare: true,
+        isStrike: false,
+      },
+    ];
+
+    expect(createFrames(framesAsString)).toEqual(expectedFrames);
+  });
+
   it("it should create a set of 12 frames with 1 turn and non strike if 10th and 11th frame is a strike", () => {
     const framesAsString = "5/3 -- 5/ X 3/ 3/ 5/2 2- 5/4 X X 4";
 
